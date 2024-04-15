3112a9b8fe80c8962eb935cab32de648bd86eb86
3uomlkh <cw1205@duksung.ac.kr>
2024-04-15 22:05:56 +0900

    add: Bronze III 5문제

---
+++ chaewon/bronze3/팰린드롬인지_확인하기.java
@@ -0,0 +1,15 @@
+package bronze3;
+
+import java.io.*;
+public class 팰린드롬인지_확인하기 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder(br.readLine());
+
+        if (sb.toString().contentEquals(sb.reverse())) {
+            System.out.println(1);
+        } else {
+            System.out.println(0);
+        }
+    }
+}

0cb39dea3fba657349b3b2dcbe200513de2819b7
3uomlkh <cw1205@duksung.ac.kr>
2024-04-16 17:49:35 +0900

    add: Bronze III 5문제

---
+++ chaewon/bronze3_cw/공_넣기.java
@@ -0,0 +1,33 @@
+package bronze3_cw;
+
+import java.io.*;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+public class 공_넣기 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        int[] basket = new int[N];
+
+        for (int i = 0; i < M; i++) {
+            st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            int c = Integer.parseInt(st.nextToken());
+
+            for (int j = a; j <= b; j++) {
+                basket[j - 1] = c;
+            }
+        }
+
+        StringBuilder sb = new StringBuilder();
+        for (int b : basket) {
+            sb.append(b).append(" ");
+        }
+
+        System.out.println(sb);
+    }
+}
---
+++ chaewon/bronze3_cw/행렬_덧셈.java
@@ -0,0 +1,42 @@
+package bronze3_cw;
+
+import java.io.*;
+import java.util.*;
+public class 행렬_덧셈 {
+    static int[][] arr;
+    static int[][] arr2;
+    static int[][] result;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+
+        arr = new int[N][M];
+        arr2 = new int[N][M];
+        result = new int[N][M];
+
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < M; j++) {
+                arr[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < M; j++) {
+                arr2[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < M; j++) {
+                System.out.print(arr[i][j] + arr2[i][j] + " ");
+            }
+            System.out.println();
+        }
+
+    }
+}
